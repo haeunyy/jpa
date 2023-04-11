659b58ed37241a5c87d6fe45240fde3a42b7630d
KWON HA EUN <120347036+haeunyy@users.noreply.github.com>
2023-04-11 17:55:41 +0900

    Add files via upload
    JPA test

--- chap001/src/com/greedy/employee/EmployeeAndDepartementTests.java
+++ chap001/src/com/greedy/employee/EmployeeAndDepartementTests.java
@@ -1,18 +1,23 @@
 package com.greedy.employee;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 
 public class EmployeeAndDepartementTests {
 
@@ -75,20 +80,83 @@ public class EmployeeAndDepartementTests {
 		Employee foundEmp = entityManager.find(Employee.class, emp.getEmpId());
 		assertEquals(emp.getEmpId(), foundEmp.getEmpId());
 		System.out.println(foundEmp);
+	}
+	
+	@Test
+	public void 삭제() {
 		
+		Employee emp = entityManager.find(Employee.class, "250");
+		
+		EntityTransaction trans = entityManager.getTransaction();
+		trans.begin();
+		entityManager.remove(emp);
+		trans.commit();
+		
+		assertEquals(null, entityManager.find(Employee.class, "250"));
+	}
+	
+	@Test
+	public void 새로운_부서_등록_테스트() {
+		
+		Employee emp = new Employee();
+		emp.setEmpId("250");
+		emp.setEmpNo("940625-2034967");
+		emp.setEmpName("테스트");
+		emp.setPhone("01001011919");
+		emp.setEmail("ididi@gmail.com");
+		emp.setJobCode("J1");
+		emp.setSalLevel("S1");
+		
+		Department dept = new Department();
+		dept.setDeptId("55");
+		dept.setDeptTitle("테스트부서");
+		dept.setLocationId("L1");
+		
+		emp.setDept(dept);
+		
+		EntityTransaction trans = entityManager.getTransaction();
+		trans.begin();
+		entityManager.persist(emp);
+		trans.commit();
+		
+		Employee foundemp = entityManager.find(Employee.class, emp.getEmpId());
+		assertEquals(emp.getEmpId(), foundemp.getEmpId());
+		assertEquals(dept.getDeptId(), foundemp.getDept().getDeptId());
+		System.out.println(foundemp);
 	}
 	
 	
 	
+	@Test
+	public void 모두모두집합조회_테스트() {
+		
+		//when
+		String jpql = "SELECT e FROM employee as e";
+		Query query = entityManager.createQuery(jpql);	
+		
+		List<Employee> foundEmpList = query.getResultList();
+		
+		//then
+		assertNotNull(foundEmpList);
+		foundEmpList.forEach(System.out::println);
+		
+	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
+	@Test
+	public void 자네하씨성을가졌나_조회_테스트() {
+		
+		//when
+		String jpql = "SELECT e FROM employee as e WHERE e.empName LIKE '하' || '%' ";
+		Query query = entityManager.createQuery(jpql);	
+		
+		List<Employee> foundEmpList = query.getResultList();
+		
+		//then
+		assertNotNull(foundEmpList);
+		foundEmpList.forEach(System.out::println);
+		
+	}
 	
 	
 	
